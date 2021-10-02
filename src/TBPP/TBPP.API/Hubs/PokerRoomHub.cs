b7fbf62b0019498934ec90b10fe1132bb089dbaa
Will Holmes <willholmes97@icloud.com>
2021-10-02 23:13:57 +0100

    Initial Commit

---
+++ src/TBPP/TBPP.API/Hubs/PokerRoomHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TBPP.API.Hubs
+{
+    public class PokerRoomHub : Hub
+    {
+    }
+}
